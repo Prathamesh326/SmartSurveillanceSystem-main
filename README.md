d19400c3d30498b7a485c02832f668e1fe737b85
Prathamesh326 <prathameshshinde326@gmail.com>
2024-10-13 12:54:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SmartSurveillanceSystem
+This is our Final Year Project that includes detecting various anomalies like fall detection, vehicle crash detection, involving smart monitoring including object detection and various analysis of algorithms.
---
+++ image_email_fall.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+def SendMail(ImgFileName):
+    img_data = open(ImgFileName, 'rb').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Fall Alert'
+    msg['From'] = 'dikshantbuwa1@gmail.com.cc'
+    msg['To'] = 'dikshantbuwa2@gmail.com.cc'
+
+    text = MIMEText("Fall Detected. Send HELP")
+    msg.attach(text)
+    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+    msg.attach(image)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login("dikshantbuwa1@gmail.com", "riandtnihimyqbap")
+    s.sendmail("dikshantbuwa1@gmail.com", "dikshantbuwa2@gmail.com", msg.as_string())
+    s.quit()
